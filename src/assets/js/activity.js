c38d4223f651763d2edff512fd36b53b1e2e895e
Matheus Reis <matheus.reisluz@gmail.com>
2020-09-28 16:19:11 -0300

    Adicionado modal

--- src/assets/js/activity.js
+++ src/assets/js/activity.js
@@ -1,12 +1,17 @@
 var modal = document.getElementById("modal-activity");
 var btn = document.getElementById("btn-activity");
 var span = document.getElementsByClassName("close")[0];
+var close = document.getElementsByClassName("close-btn")[0];
 
 btn.onclick = function() {
     modal.style.display = "flex";
 }
 
 span.onclick = function() {
+    modal.style.display = "none";
+}
+
+close.onclick = function() {
     modal.style.display = "none";
 }
 
