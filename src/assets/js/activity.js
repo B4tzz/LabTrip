0b0f6e868f0e1903f86e1fc2c102530fb0a6c30e
Matheus Reis <matheus.reisluz@gmail.com>
2020-09-28 11:46:40 -0300

    Adicionado pagina Activity.html - incompleta

---
+++ src/assets/js/activity.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById("modal-activity");
+var btn = document.getElementById("btn-activity");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
