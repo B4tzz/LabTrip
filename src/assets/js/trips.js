064835f69d1c40f4a6079d47dce73bb50184843c
Matheus Reis <matheus.reisluz@gmail.com>
2020-09-29 16:44:59 -0300

    Ajuste no arquivo trips.js

--- src/assets/js/trips.js
+++ src/assets/js/trips.js
@@ -1,11 +1,11 @@
 var modal = document.getElementById("modal-trip");
-var btn = document.querySelectorAll(".btn-edit");
+var btn = document.getElementsByClassName("btn-edit");
 var span = document.getElementsByClassName("close")[0];
 var close = document.getElementsByClassName("close-btn")[0];
 
 
 for(i=0; i < btn.length; i++){
-    btn[1].onclick = function() {
+    btn[i].onclick = function() {
         modal.style.display = "flex";
     }
 }
