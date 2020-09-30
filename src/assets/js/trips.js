9e7a11a3098aa5e046a99f99da589b1a0ea66cf5
Gabriel Batista <gabriel.goncalves@libbs.com.br>
2020-09-29 23:33:48 -0300

    Adicionando modal para deletar e ajustando orientacao e objetos

--- src/assets/js/trips.js
+++ src/assets/js/trips.js
@@ -1,21 +1,37 @@
 var modal = document.getElementById("modal-trip");
-var btn = document.getElementsByClassName("btn-edit");
-var span = document.getElementsByClassName("close")[0];
-var close = document.getElementsByClassName("close-btn")[0];
+var modalEdit = document.getElementById("modal-edit");
+var modalDelete = document.getElementById("modal-delete");
+var btnEdit = document.getElementsByClassName("btn-edit");
+var btnDelete = document.getElementsByClassName("btn-delete");
+var span = document.getElementsByClassName("close");
+var close = document.getElementsByClassName("close-btn");
 
-
-for(i=0; i < btn.length; i++){
-    btn[i].onclick = function() {
+for(i=0; i < btnEdit.length; i++){
+    btnEdit[i].onclick = function() {
         modal.style.display = "flex";
+        modalEdit.style.display = "flex";
+        modalDelete.style.display = "none";
     }
 }
 
-span.onclick = function() {
-    modal.style.display = "none";
+for(i=0; i < btnDelete.length; i++){
+    btnDelete[i].onclick = function() {
+        modal.style.display = "flex";
+        modalEdit.style.display = "none";
+        modalDelete.style.display = "unset";
+    }
 }
 
-close.onclick = function() {
-    modal.style.display = "none";
+for(i=0; i < span.length; i++){
+    span[i].onclick = function() {
+        modal.style.display = "none";
+    }
+}
+
+for(i=0; i < close.length; i++){
+    close[i].onclick = function() {
+        modal.style.display = "none";
+    }
 }
 
 window.onclick = function(event) {
