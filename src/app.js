2276f8e05b5780eebdf54cacf7186f4244a43b15
B4tzz <biel0828@hotmail.com>
2020-10-01 01:56:56 +0000

    Adicionando classes para API crud de viagens e inclusão das classes no app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import defineUsuarioRouter from './usuarios/UsuarioRouter'
+import defineViagemRouter from './viagens/ViagemRouter'
 
 export default function LabTrip() {
   const app = express();
   app.use(express.json());
   app.use(cors());
   app.use('/usuarios', defineUsuarioRouter());
+  app.use('/trips', defineViagemRouter());
 
   app.get('/', function(req, res) {
 
