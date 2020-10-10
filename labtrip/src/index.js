c70343eeb5446ae13084cb4c818a1db91fdf8070
B4tzz <biel0828@hotmail.com>
2020-10-10 01:49:20 +0000

    adicionando page trips

--- labtrip/src/index.js
+++ labtrip/src/index.js
@@ -4,12 +4,46 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import express from 'express';
+import cors from 'cors';
+import UsuarioController from './usuarios/UsuarioController'
+import UsuarioMiddleware from './usuarios/UsuarioMiddleware'
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const usuarios = [
+  {id: 1, nome: 'Maria'},
+  {id: 2, nome: 'João' }
+];
+
+const usuarioController =  new UsuarioController(usuarios);
+const usuarioMiddleware =  new UsuarioMiddleware(usuarios);
+
+app.route('/usuarios')
+.get((req, res) => usuarioController.index(req,res))
+.post((req, res) => usuarioController.save(req, res))
+
+app.route('/usuarios/:id')
+.all((req, res, next) => usuarioMiddleware.usuarioExiste(req, res, next))
+.get((req, res) => usuarioController.show(req, res))
+.put((req, res) => usuarioController.update(req, res))
+.delete((req, res) => usuarioController.delete(req, res))
+
+
+
+app.get('/', function(req, res){
+    ReactDOM.render(
+        <App />,
+      document.getElementById('root')
+    );
+});
+
+app.listen(3000, function(){
+        console.log('Rodando na porta 3000!');
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ labtrip/src/pages/trips.jsx
@@ -0,0 +1,306 @@
+import React from 'react'
+
+export default () =>{
+
+  <header class="head">
+        <h1>Trips</h1>
+    </header>
+
+    <main class="main-section">
+        <div class="main-header">
+            <h3>Gerencie suas viagens</h3>
+        </div>
+        <div class="create-new-container">
+            <div class="item-container">
+                    <div class="create-item-container">
+                        <div class="create-item">
+                            <img id="btn-new" src="../assets/images/mais.svg" />
+                        </div>
+                    </div>
+            </div>
+        </div>
+        <div class="main-content">
+            <div class="item-container">
+                <div class="item">
+
+                    <a href="#">
+                        <div class="item-left">
+                            <div class="item-title">
+                                <p>Title</p>
+                            </div>
+                            <div class="line-divisory"></div>
+                            <div class="item-box">
+                                <div class="item-index">1</div>
+                                <div class="item-content">
+                                    <ul>
+                                        <li>
+                                            <p>Início: 20/09/2020</p>
+                                        </li>
+                                        <li>
+                                            <p>Fim: 27/09/2020</p>
+                                        </li>
+                                        <li>
+                                            <p>Concluída</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </a>
+                    <div class="item-options">
+                        <div class="item-options-trash">
+                            <span class="btn-delete">
+                                <i class="fa fa-trash"></i>
+                            </span>
+                        </div>
+                        <div class="item-options-pencil">
+                            <span class="btn-edit">
+                                <p>&#128397;</p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item-container">
+                <div class="item">
+
+                    <a href="#">
+                        <div class="item-left">
+                            <div class="item-title">
+                                <p>Title</p>
+                            </div>
+                            <div class="line-divisory"></div>
+                            <div class="item-box">
+                                <div class="item-index">2</div>
+                                <div class="item-content">
+                                    <ul>
+                                        <li>
+                                            <p>Início: 20/09/2020</p>
+                                        </li>
+                                        <li>
+                                            <p>Fim: 27/09/2020</p>
+                                        </li>
+                                        <li>
+                                            <p>Concluída</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </a>
+                    <div class="item-options">
+                        <div class="item-options-trash">
+                            <span class="btn-delete">
+                                <i class="fa fa-trash"></i>
+                            </span>
+                        </div>
+                        <div class="item-options-pencil">
+                            <span class="btn-edit">
+                                <p>&#128397;</p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item-container">
+                <div class="item">
+
+                    <a href="#">
+                        <div class="item-left">
+                            <div class="item-title">
+                                <p>Title</p>
+                            </div>
+                            <div class="line-divisory"></div>
+                            <div class="item-box">
+                                <div class="item-index">3</div>
+                                <div class="item-content">
+                                    <ul>
+                                        <li>
+                                            <p>Início: 20/09/2020</p>
+                                        </li>
+                                        <li>
+                                            <p>Fim: 27/09/2020</p>
+                                        </li>
+                                        <li>
+                                            <p>Concluída</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </a>
+                    <div class="item-options">
+                        <div class="item-options-trash">
+                            <span class="btn-delete">
+                                <i class="fa fa-trash"></i>
+                            </span>
+                        </div>
+                        <div class="item-options-pencil">
+                            <span class="btn-edit">
+                                <p>&#128397;</p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item-container">
+                <div class="item">
+
+                    <a href="#">
+                        <div class="item-left">
+                            <div class="item-title">
+                                <p>Title</p>
+                            </div>
+                            <div class="line-divisory"></div>
+                            <div class="item-box">
+                                <div class="item-index">4</div>
+                                <div class="item-content">
+                                    <ul>
+                                        <li>
+                                            <p>Início: 20/09/2020</p>
+                                        </li>
+                                        <li>
+                                            <p>Fim: 27/09/2020</p>
+                                        </li>
+                                        <li>
+                                            <p>Concluída</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </a>
+                    <div class="item-options">
+                        <div class="item-options-trash">
+                            <span class="btn-delete">
+                                <i class="fa fa-trash"></i>
+                            </span>
+                        </div>
+                        <div class="item-options-pencil">
+                            <span class="btn-edit">
+                                <p>&#128397;</p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item-container">
+                <div class="item">
+
+                    <a href="#">
+                        <div class="item-left">
+                            <div class="item-title">
+                                <p>Title</p>
+                            </div>
+                            <div class="line-divisory"></div>
+                            <div class="item-box">
+                                <div class="item-index">5</div>
+                                <div class="item-content">
+                                    <ul>
+                                        <li>
+                                            <p>Início: 20/09/2020</p>
+                                        </li>
+                                        <li>
+                                            <p>Fim: 27/09/2020</p>
+                                        </li>
+                                        <li>
+                                            <p>Concluída</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </a>
+                    <div class="item-options">
+                        <div class="item-options-trash">
+                            <span class="btn-delete">
+                                <i class="fa fa-trash"></i>
+                            </span>
+                        </div>
+                        <div class="item-options-pencil">
+                            <span class="btn-edit">
+                                <p>&#128397;</p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <div class="modal" id="modal-trip">
+            <div class="modal-content">
+                <div class="modal-delete" id="modal-delete">
+                    <div class="modal-header">
+                        <h2>Deletar viagem ${Viagem}</h2>
+                        <span class="close">×</span>
+                    </div>
+                    <div class="modal-body">
+                        <div class="modal-title">
+                            <p>Tem certeza que deseja deletar esta viagem?</p>
+                            <button class="close-btn">Cancelar</button>
+                            <button class="confirm-btn">Confirmar</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-edit" id="modal-edit">
+                    <div class="modal-header">
+                        <h2>Edite sua viagem</h2>
+                        <span class="close">×</span>
+                    </div>
+                    <div class="modal-body">
+                        <div class="modal-title">
+                            <label for="title">Título</label><br>
+                            <input id="title" name="title" type="text" />
+                        </div>
+                        <div class="modal-dates">
+                            <div class="start-date">
+                                <label for="start-date">Data início</label><br>
+                                <input id="start-date" name="start-date" type="date" />
+                            </div>
+                            <div class="end-date">
+                                <label for="start-date">Data fim</label><br>
+                                <input id="start-date" name="start-date" type="date" />
+                            </div>                            
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="close-btn">Cancelar</button>
+                        <button class="confirm-btn">Confirmar</button>
+                    </div>
+                </div>
+                <div class="modal-new" id="modal-new">
+                    <div class="modal-header">
+                        <h2>Crie sua viagem</h2>
+                        <span class="close">×</span>
+                    </div>
+                    <div class="modal-body">
+                        <div class="modal-title">
+                            <label for="title">Título</label><br>
+                            <input id="title" name="title" type="text" />
+                        </div>
+                        <div class="modal-dates">
+                            <div class="start-date">
+                                <label for="start-date">Data início</label><br>
+                                <input id="start-date" name="start-date" type="date" />
+                            </div>
+                            <div class="end-date">
+                                <label for="start-date">Data fim</label><br>
+                                <input id="start-date" name="start-date" type="date" />
+                            </div>                            
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="close-btn">Cancelar</button>
+                        <button class="confirm-btn">Confirmar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <script src="../assets/js/trips.js"></script>
+  
+}
